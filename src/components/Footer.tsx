b1cc8d28563a66bf654015f8f0c6e6e64062f7bc
Abhinandana Bhatta <abhivocopedia@gmail.com>
2026-09-21 05:41:33 +0530

    Fix duplicate entry for Coca-Cola in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,9 +51,9 @@ const footerToolLogos = [
   ['Nvim', 'https://neovim.io/', '/icons/neovim.svg'],
   ['Linux', 'https://www.linux.org/', '/icons/linux.svg'],
   ['Windows', 'https://www.microsoft.com/windows/', '/icons/windows.svg'],
-  ['WSL', 'https://learn.microsoft.com/windows/wsl/', '/icons/wsl.svg'],  ['Coca-Cola', 'https://www.coca-cola.com/', '/icons/cocacola.svg'],
+  ['WSL', 'https://learn.microsoft.com/windows/wsl/', '/icons/wsl.svg'],  
+  ['Coca-Cola', 'https://www.coca-cola.com/', '/icons/cocacola.svg'],
   ['Red Bull', 'https://www.redbull.com/', '/icons/redbull.svg'],
-  ['Coca-Cola', 'https://www.coca-cola.com/', '/icons/coca-cola.svg'],
   ['Diet Coke', 'https://www.coca-cola.com/', '/icons/diet-coke.svg'],] as const
 
 export function Footer() {
